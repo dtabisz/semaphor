a5579a2017508caf8b9e53c1676e81a1794c4757
dtabisz <35840883+dtabisz@users.noreply.github.com>
2023-04-05 21:35:17 +0200

    Add files via upload
    This part turns input string onto ascii-art depicting position of flags

---
+++ SEMAFORY01.py
@@ -0,0 +1,389 @@
+#text input/or string from semafory_prekod
+
+# message creation
+#message is list/string that contains UPPER A-Z, NUM, " " (space) 
+
+print('enter precoded midlist')
+mid = input()
+midlist = mid.upper()
+message = mid.upper()
+
+#flag definitions
+#a =
+def semafor_a ():
+    print("")
+    print("")
+    print("")
+    print("   O ")
+    print("  /| ")
+    print(" / |  ")
+    print("#  # ")
+    print("_______\n")
+
+#b =
+def semafor_b ():
+    print("")
+    print("")
+    print("")
+    print("    O ")
+    print("#---| ")
+    print("    |  ")
+    print("    # ")
+    print("_______\n")
+
+# c =
+def semafor_c ():
+    print("")
+    print("#\\")
+    print("  \\")
+    print("   \\O ")
+    print("    | ")
+    print("    |  ")
+    print("    # ")
+    print("_______\n")
+
+# d = 
+def semafor_d ():
+    print("  #|")
+    print("   |")
+    print("   | ")
+    print("    O ")
+    print("    | ")
+    print("    |  ")
+    print("    # ")
+    print("_______\n")
+
+# e =
+def semafor_e ():
+    print("")
+    print("      /#")
+    print("     /")
+    print("    O ")
+    print("    | ")
+    print("    |  ")
+    print("    # ")
+    print("_______\n")
+
+#f =
+def semafor_f ():
+    print("")
+    print("")
+    print("")
+    print("    O---#")
+    print("    | ")
+    print("    |  ")
+    print("    # ")
+    print("_______\n")
+#g=
+def semafor_g ():
+    print("")
+    print("")
+    print("")
+    print("    O")
+    print("    |\\ ")
+    print("    | \\")
+    print("    #  #")
+    print("_______\n")
+
+#h = 
+def semafor_h ():
+    print("")
+    print("")
+    print("")
+    print("    O ")
+    print("#---/ ")
+    print("   /.   ")
+    print("  # .  ")
+    print("_______\n")
+
+#i =
+def semafor_i ():
+    print("")
+    print("#\\")
+    print("  \\")
+    print("    O ")
+    print("    / ")
+    print("   / .  ")
+    print("  #  . ")
+    print("_______\n")
+
+#j = #letters
+def semafor_j ():
+    print("  #|")
+    print("   |")
+    print("   | ")
+    print("    O---# ")
+    print("    . ")
+    print("    .  ")
+    print("    . ")
+    print("_______\n")
+
+#k 
+def semafor_k ():
+    print("  #|")
+    print("   |")
+    print("   |")
+    print("   O")
+    print("  /.  ")
+    print(" / .    ")
+    print(" # .   ")
+    print("_______\n")
+
+#l 
+def semafor_l ():
+    print("")
+    print("     /#")
+    print("    /")
+    print("   O ")
+    print("  /.   ")
+    print(" / .    ")
+    print("#  .   ")
+    print("_______\n")
+
+#m 
+def semafor_m ():
+    print("")
+    print("")
+    print("")
+    print("   O---# ")
+    print("  /. ")
+    print(" / .  ")
+    print("#  . ")
+    print("_______\n")
+
+#n 
+def semafor_n ():
+    print("")
+    print("")
+    print("")
+    print("   O ")
+    print("  /.\\ ")
+    print(" / . \\ ")
+    print("#  .  #")
+    print("_______\n")
+
+#o 
+def semafor_o ():
+    print("")
+    print(" #\\")
+    print("   \\")
+    print("#---0 ")
+    print("    .  ")
+    print("    . ")
+    print("    . ")
+    print("_______\n")
+
+#p 
+def semafor_p ():
+    print("    #| ")
+    print("     |")
+    print("     |")
+    print("#---O ")
+    print("    .  ")
+    print("    . ")
+    print("    . ")
+    print("_______\n")
+
+#q 
+def semafor_q ():
+    print("")
+    print("       /# ")
+    print("      /")
+    print("#---O ")
+    print("    .  ")
+    print("    . ")
+    print("    . ")
+    print("_______\n")
+
+#r 
+def semafor_r ():
+    print("")
+    print("")
+    print("")
+    print("#---O---# ")
+    print("    .  ")
+    print("    . ")
+    print("    . ")
+    print("_______\n")
+
+#S 
+def semafor_s ():
+    print("")
+    print("")
+    print("")
+    print("#---O ")
+    print("    .\\  ")
+    print("    . \\ ")
+    print("    . #\\")
+    print("_______\n")
+
+#t
+def semafor_t ():
+    print("    #|") 
+    print("#\\   |")
+    print("  \\  |")
+    print("   \\O ")
+    print("    . ")
+    print("    .  ")
+    print("    . ")
+    print("_______\n ")
+
+#u 
+def semafor_u ():
+    print("")
+    print("#\\     /#")
+    print("  \\   /")
+    print("   \\O/ ")
+    print("    . ")
+    print("    .  ")
+    print("    . ")
+    print("_______\n")
+
+#v
+def semafor_v ():
+    print("   #| ")
+    print("    |")
+    print("    |")
+    print("    O ")
+    print("    .\\  ")
+    print("    . \\")
+    print("    . #\\")      
+    print("_______\n")
+
+
+#w 
+def semafor_w ():
+    print("")
+    print("    /#")
+    print("   /")
+    print("  O---# ")
+    print("  .   ")
+    print("  .   ")
+    print("  . ")
+    print("_______\n")
+
+#x print("_______")
+def semafor_x ():
+    print("")
+    print("    /#")
+    print("   /")
+    print("  O ")
+    print("  .\\   ")
+    print("  . \\   ")
+    print("  . #\\ ")
+    print("_______\n")
+
+#y print("_______")
+def semafor_y ():
+    print("     ")
+    print("#\\")
+    print("  \\")
+    print("   \\O---#")
+    print("    .")
+    print("    .")
+    print("    .")
+    print("_______")
+
+#z 
+def semafor_z ():
+    print("")
+    print("")
+    print("")
+    print("    O---# ")
+    print("    .\\  ")
+    print("    . \\ ")
+    print("    . #\\")
+    print("_______\n")
+
+
+#[space] print("_______")
+def semafor_space ():
+    print("")
+    print("")
+    print("")
+    print("   O")
+    print("  |.|")
+    print("  |.|")
+    print(" #|.|#")
+    print("________\n")
+
+#[numbers] 
+def semafor_num ():
+    print("      /# ")
+    print("     /")
+    print("    O---# ")
+    print("    .  ")
+    print("    . ")
+    print("    . ")
+    print("_______")
+
+#[attention] print("_______") print("_______")
+
+#[cancel] print("_______")
+
+
+
+#flag display code
+print(message)
+for c in message:
+    print(c)
+    if c == "A":
+        semafor_a()
+    if c == "B":
+        semafor_b()
+    if c == "C":
+        semafor_c()
+    if c == "D":
+        semafor_d()
+    if c == "E":
+        semafor_e()
+    if c == "F":
+        semafor_f()
+    if c == "G":
+        semafor_g()
+    if c == "H":
+        semafor_h()
+    if c == "I":
+        semafor_i()
+    if c == "J":
+        semafor_j()
+    if c == "K":
+        semafor_k()
+    if c == "L":
+        semafor_l()
+    if c == "M":
+        semafor_m()
+    if c == "N":
+        semafor_n()
+    if c == "O":
+        semafor_o()
+    if c == "P":
+        semafor_p()
+    if c == "Q":
+        semafor_q()
+    if c == "R":
+        semafor_r()
+    if c == "S":
+        semafor_s()
+    if c == "T":
+        semafor_t()
+    if c == "U":
+        semafor_u()
+    if c == "V":
+        semafor_v()
+    if c == "W":
+        semafor_w()
+    if c == "X":
+        semafor_x()
+    if c == "Y":
+        semafor_y()
+    if c == "Z":
+        semafor_z()
+    if c == " ":
+        semafor_space()
+    if c == "NUM":
+        semafor_num()
+    if c == "#":    #obejście problemu num
+        semafor_num()
+    if c == "LIT": # "LIT" = "J"
+        semafor_j
